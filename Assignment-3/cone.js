9612ec5dc8871ad6ada777757d6684ab72627866
Snore-Relax <67124641+Snore-Relax@users.noreply.github.com>
2021-01-26 17:39:28 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -1,22 +1,26 @@
 var gl = null;
+var cone = null;
 
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
 
+    cone = new Cone(gl, 21);
+
     if ( !gl ) {
         alert("Unable to setup WebGL");
         return;
     }
 
-    gl.clearColor( 1, 0.5, 0.5, 3);
+    //gl.clearColor( 1, 0.5, 0.5, 3);
 
     render();
 }
 
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
+    //gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
 }
 
 window.onload = init;
