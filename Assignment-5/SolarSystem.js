cd2e924b565811c91968a874c09c53a7ca164669
Snore-Relax <67124641+Snore-Relax@users.noreply.github.com>
2021-02-11 09:50:05 -0800

    Add files via upload

---
+++ Assignment-5/SolarSystem.js
@@ -0,0 +1,68 @@
+var SolarSystem = {
+  Sun : {
+    radius : 10,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 1.0553,
+    distance : 0.387,
+    year : 0.241,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Venus : {
+    radius : 2.949,
+    distance : 0.723,
+    year : 0.615,
+    color : [ 1.0, 0.0, 1.0, 1.0 ]
+  },
+  Earth : {
+    radius : 2,
+    distance : 1,
+    year : 1,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Moon : {
+    radius : 1.2724,
+    distance : 0.00257,
+    year : 0.0748,
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  Mars : {
+    radius : 3.532,
+    distance : 1.52,
+    year : 1.88,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Jupiter : {
+    radius : 9.21,
+    distance : 5.20,
+    year : 11.9,
+    color : [ 1.0, 153/255, 0.0, 1.0 ]
+  },
+  Saturn : {
+    radius : 8.45,
+    distance : 9.58,
+    year : 29.4,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Uranus : {
+    radius : 4.91,
+    distance : 19.20,
+    year : 83.7,
+    color : [ 0.0, 0.1, 1.0, 1.0 ]
+  },
+  Neptune : {
+    radius : 4.08,
+    distance : 30.05,
+    year : 163.7,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.5,
+    distance : 39.48,
+    year : 247.9,
+    color : [ 150/255, 75/255, 0.0, 1.0 ]
+  }
+};
